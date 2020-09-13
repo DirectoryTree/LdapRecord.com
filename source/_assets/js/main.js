1c8ebd8c6610eda9f89245845850c0a651b3e0aa
Steve Bauman <steven_bauman@outlook.com>
2020-09-13 19:13:04 -0400

    Some more layout tweaks

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -22,7 +22,7 @@ const app = new Vue({
 
 $(document).ready(() => {
     // Wrap tables in content responsive container.
-    $('.content > table').wrap($("<div />").addClass('block shadow overflow-auto rounded-lg'));
+    $('.content > table').wrap($("<div />").addClass('block border overflow-auto rounded-lg'));
 
     $('.content > pre[class*="language-"').each((index, el) => {
         let language = el.classList[0].split('-')[1];
