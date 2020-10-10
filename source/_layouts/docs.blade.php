f640509bcc08cf4be52543c47adde96412ddd62b
Steve Bauman <steven_bauman@outlook.com>
2020-10-10 14:08:46 -0400

    Make navbar sticky

--- source/_layouts/docs.blade.php
+++ source/_layouts/docs.blade.php
@@ -7,7 +7,7 @@
 @section('body')
     <section class="container relative max-w-8xl mx-auto px-6 md:px-8 py-4">
         <div class="flex flex-col lg:flex-row justify-center">
-            <nav id="js-nav-menu" class="nav-menu hidden lg:block">
+            <nav id="js-nav-menu" class="nav-menu relative hidden lg:block">
                 @yield('nav')
             </nav>
 
--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,6 +1,6 @@
 @php $level = $level ?? 0 @endphp
 
-<ul class="list-none m-0 {{ $level > 0 && count($items) > 1 ? '' : '' }}">
+<ul class="list-none md:sticky md:top-0 m-0 {{ $level > 0 && count($items) > 1 ? '' : '' }}">
     @foreach ($items as $label => $item)
         @include('_nav.menu-item', ['first' => $loop->first])
     @endforeach
