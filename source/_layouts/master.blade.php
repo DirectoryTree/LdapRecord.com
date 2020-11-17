3b04489c3d429bf7648505bed0e4e50b721bead2
Steve Bauman <steven_bauman@outlook.com>
2020-11-17 18:32:39 -0500

    Make sure we close all the meta tags

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,32 +6,32 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
 
-        <meta property="og:site_name" content="{{ $page->siteName }}"/>
-        <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
-        <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}"/>
-        <meta property="og:url" content="{{ $page->getUrl() }}"/>
-        <meta property="og:image" content="/assets/img/logo.png"/>
-        <meta property="og:type" content="website"/>
+        <meta property="og:site_name" content="{{ $page->siteName }}" />
+        <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}" />
+        <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
+        <meta property="og:url" content="{{ $page->getUrl() }}" />
+        <meta property="og:image" content="/assets/img/logo.png" />
+        <meta property="og:type" content="website" />
 
         <!-- Twitter Meta -->
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:image" content="/assets/img/logo.png">
-        <meta name="twitter:image:alt" content="{{ $page->siteName }}">
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/assets/img/logo.png" />
+        <meta name="twitter:image:alt" content="{{ $page->siteName }}" />
 
         <!-- Favicons -->
-        <link rel="icon" href="/favicon.ico">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#805ad5">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <meta name="msapplication-TileColor" content="#805ad5">
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#805ad5" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="msapplication-TileColor" content="#805ad5" />
 
         @stack('meta')
 
         <title>{{ $page->siteName }}{{ $page->title ? ' | ' . $page->title : '' }}</title>
-        <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="home" href="{{ $page->baseUrl }}" />
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}" />
 
         @if ($page->docsearchApiKey && $page->docsearchIndexName)
             <link rel="preload" as="style" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" onload="this.onload=null;this.rel='stylesheet'"/>
